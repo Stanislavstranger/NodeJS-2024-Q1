238cec87d731a3dad1d0dbfac8b8e331ff35f207
Stanislav <stanislavstranger@gmail.com>
2024-02-05 12:33:03 +0300

    fix: fix goUp

--- src/utils/goUp.js
+++ src/utils/goUp.js
@@ -4,12 +4,20 @@ import { printCurrentDirectory } from './printCurrentDirectory.js';
 import printOperationFailed from './printOperationFailed.js';
 import globalVars from '../globalVars/globalVars.js';
 
-export default function goUp() {
+export default async function goUp() {
   const parent = path.resolve(globalVars.currentWorkingDirectory, '..');
-  if (parent !== globalVars.currentWorkingDirectory && fs.existsSync(parent) && fs.statSync(parent).isDirectory()) {
-    globalVars.currentWorkingDirectory = `${parent}`;
-    printCurrentDirectory(globalVars.currentWorkingDirectory);
-  } else {
+
+  try {
+    await fs.promises.access(parent, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK);
+    const stats = await fs.promises.stat(parent);
+
+    if (stats.isDirectory()) {
+      globalVars.currentWorkingDirectory = parent;
+      printCurrentDirectory(globalVars.currentWorkingDirectory);
+    } else {
+      printOperationFailed();
+    }
+  } catch (error) {
     printOperationFailed();
   }
 }
